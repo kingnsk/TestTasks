d1bcca1a583f2d6e644ad44c26083fe77a0bc3b7
kingnsk <kingnsk2020@gmail.com>
2023-05-18 22:47:32 +0700

    Release UnitTests

---
+++ AreaCalculationLibrary/AreaCalc.Tests/CircleTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AreaCalc.Tests
+{
+    public class CircleTests
+    {
+
+        [Theory]
+        [InlineData(10.0)]
+        [InlineData(1.0)]
+    
+        public void AreaCalcTest(double radius)
+        {
+            var circle = new Circle(radius);
+            var expectedArea = Math.PI*Math.Pow(radius,2.0);
+            Assert.Equal(expectedArea, circle.Area);
+        }
+
+        [Theory]
+        [InlineData(-100)]
+        public void NegativeRadiusThrowExceptionTest(double radius)
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => new Circle(radius));
+        }
+    
+    }
+}
---
+++ AreaCalculationLibrary/AreaCalc.Tests/TriangleTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AreaCalc.Tests
+{
+    public class TriangleTests
+    {
+        [Theory]
+        [InlineData(15.0, 8.0, 17.0, 60.0)]
+        public void AreaCalcTest(double a, double b, double c, double expectedArea)
+        {
+           var triangle = new Triangle(a,b,c);
+            Assert.Equal(expectedArea, triangle.Area);
+
+        }
+
+        [Theory]
+        [InlineData(6.0, 8.0, 10.0, true)]
+        [InlineData(12.0, 12.0, 12.0, false)]
+        public void IsRightTriangleTest(double a, double b, double c, bool isRigtTriangle)
+        {
+            var triangle = new Triangle(a, b, c);
+            Assert.Equal(triangle.IsRightTriangle, isRigtTriangle);
+        }
+
+        [Theory]
+        [InlineData(6.0, 8.0, 77.0)]
+        public void IsNotTriangleThrowExceptionTest(double a, double b, double c)
+        {
+            Assert.Throws<ArgumentException>(() => new Triangle(a,b,c));
+        }
+
+        [Theory]
+        [InlineData(6.0, 8.0, -10.0)]
+        [InlineData(6.0, -8.0, 10.0)]
+        [InlineData(-6.0, 8.0, 10.0)]
+        public void NegativeLenghtSideThrowExceptionTest(double a, double b, double c)
+        {
+            Assert.Throws<ArgumentException>(() => new Triangle(a, b, c));
+        }
+
+
+    }
+}
